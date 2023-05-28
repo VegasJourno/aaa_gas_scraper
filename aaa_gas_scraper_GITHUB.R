51cd89a4d8981e2e6be2e4e2ffe240460633e6f7
Michael Scott Davidson <michaeldmedia@gmail.com>
2023-05-27 22:27:49 -0700

    Create aaa_gas_scraper_GITHUB.R

---
+++ aaa_gas_scraper_GITHUB.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(rvest)
+library(lubridate)
+
+aaa_nv <- read_html("https://gasprices.aaa.com/?state=NV")
+
+gas_nv <- aaa_nv %>% html_nodes(".table-mob tr:nth-child(1) td") %>%
+  html_text()
+
+gas_df <- data.frame(gas_nv)
+
+gas_df <- gas_df %>% 
+  mutate(key = rep(c('Price', 'Regular', 'Mid', 'Premium', 'Diesel'), n() / 5), 
+         id = cumsum(key == 'Price')) %>% 
+  spread(key, gas_nv) %>% 
+  select(id, Regular, Mid, Premium, Diesel) %>% 
+  mutate(Date = Sys.Date())
+
+gas_df$id[which(gas_df$id == "1")] <- "Nevada"
+gas_df$id[which(gas_df$id == "2")] <- "Las_Vegas"
+gas_df$id[which(gas_df$id == "3")] <- "Reno"
+
+##
+#Make a backup of the full scrape
+
+sys <- Sys.Date()
+
+sys <- gsub("[:]", "_", sys)
+sys <- gsub("-", "_", sys)
+
+sys <- paste0("NV_", sys, ".csv", collapse = NULL)
+
+write.csv(gas_df, file=sys,row.names=FALSE)
